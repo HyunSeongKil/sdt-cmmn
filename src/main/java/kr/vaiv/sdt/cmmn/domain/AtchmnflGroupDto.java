5704fb1580f2a426e7558f926eb1ce2dc8ed9e43
현성길 <aha1492@gmail.com>
2023-03-03 14:26:45 +0900

    init

---
+++ src/main/java/kr/vaiv/sdt/cmmn/domain/AtchmnflGroupDto.java
@@ -0,0 +1,15 @@
+package kr.vaiv.sdt.cmmn.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class AtchmnflGroupDto {
+  private String atchmnflGroupId;
+  private String bizType;
+}
