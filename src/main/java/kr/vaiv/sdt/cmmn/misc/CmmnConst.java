f1827cef60616ba8160e97c2c6c59896a574d755
HyunSeongKil <aha1492@outlook.kr>
2023-11-29 09:22:29 +0900

    Refactor CmmnConst.java: Deprecated old constants and added new ones

--- src/main/java/kr/vaiv/sdt/cmmn/misc/CmmnConst.java
+++ src/main/java/kr/vaiv/sdt/cmmn/misc/CmmnConst.java
@@ -10,10 +10,17 @@ public class CmmnConst {
     public static final String Y = "Y";
     public static final String N = "N";
 
-    public static final String DATA = "data";
+    @Deprecated(since = "20231129 @see kr.vaiv.sdt.cmmn.misc.CmmnConst#TOTAL_COUNT")
     public static final String TOTCNT = "totcnt";
-
+    @Deprecated(since = "20231129 @see kr.vaiv.sdt.cmmn.misc.CmmnConst#CODE")
     public static final String RESULT_CODE = "resultCode";
+    @Deprecated(since = "20231129 @see kr.vaiv.sdt.cmmn.misc.CmmnConst#MESSAGE")
     public static final String RESULT_MESSAGE = "resultMessage";
 
+    public static final String DATA = "data";
+    public static final String TOTAL_COUNT = "totalCount";
+    public static final String PAGE = "page";
+    public static final String SIZE = "size";
+    public static final String CODE = "code";
+    public static final String MESSAGE = "message";
 }
